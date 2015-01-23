01000a5edb7fd0b7c49c63177b4b5e52e714dcc8
Steven Sotelo <stevenbetancurt@hotmail.com>
2015-01-23 16:17:09 -0500

    add information to the links

--- maps_data/forms.py
+++ maps_data/forms.py
@@ -4,10 +4,6 @@ from maps_data.models import Map
 class MapForm(forms.Form):    
     class Meta:
         model = Map
-        file = forms.FileField()
-        error_messages = {
-            'title': {
-                'max_length': ("Field is biggest"),
-                'null': ("Field is mandatory"),
-            },
-        }
+        #file = forms.FileField()
+        #fields = ['name', 'title', 'birth_date']
+        
