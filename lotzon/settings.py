ce0deab55ebe2ca98fce8d1f8d0b9f1b50611e2a
stevensotelo <stevenbetancurt@hotmail.com>
2015-01-22 23:46:10 -0500

    create app maps

--- lotzon/settings.py
+++ lotzon/settings.py
@@ -63,10 +63,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'lcw_data':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'lcw_data.sqlite3'),
-    }
+    'default' : {
+      'ENGINE' : 'django_mongodb_engine',
+      'NAME' : 'maps'
+   }
 }
 
 # Internationalization
