4999373cb6c07a6ff39a4dc68f5de28223088255
Steven Sotelo <stevenbetancurt@hotmail.com>
2015-01-26 10:38:47 -0500

    work in the upload raster file for maps

--- maps_data/models.py
+++ maps_data/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 class Map(models.Model):
     title = models.CharField(max_length=90)    
-    
-    #def __unicode__(self):
-    #    return "Map: " + self.title + self.header
 
 class Header(models.Model):
     cols = models.IntegerField()
@@ -14,12 +11,10 @@ class Header(models.Model):
     ycorner = models.FloatField()
     cellsize = models.FloatField()    
     map = models.ForeignKey('Map')
-    
-    #def __unicode__(self):
-    #    return "Header: Cols=" + self.cols + " Rows=" + self.rows + " Type=" + self.type_raster
 
 class Row(models.Model):    
-    number = models.IntegerField()    
+    number = models.IntegerField()
+    latitude = models.FloatField()
     map = models.ForeignKey('Map')
 
 class Section(models.Model):
@@ -27,6 +22,3 @@ class Section(models.Model):
     start = models.FloatField()
     end = models.FloatField()    
     row = models.ForeignKey('Row')
-    
-    #def __unicode__(self):
-    #    return "Section: Value=" + self.value + " Start=" + self.start + " End=" + self.end
--- maps_data/views.py
+++ maps_data/views.py
@@ -7,7 +7,7 @@ from maps_data.forms import MapForm
 
 
 def index(request):
-    return render_to_response(request, 'maps_data/index.html',{'all_maps':Map.objects.all()})
+    return render(request, 'maps_data/index.html',{'all_maps':Map.objects.all()})
 
 def detail(request, id):
     return render(request, 'maps_data/detail.html', { 'map' : get_object_or_404(Map, pk=id)})
@@ -31,38 +31,60 @@ def process_file(file, map):
     cell = 0
     nodata = 0
     number_row = 1
-    for chunk in file.chunks():
+    for fileline in file:
         if line < 7:
             if line == 1:
-                cols = int(chunk.split()[1])
-            elif line == 1:
-                rows = int(chunk.split()[1])
-            elif line == 1:
-                xcorner = float(chunk.split()[1])
-            elif line == 1:
-                ycorner = float(chunk.split()[1])
-            elif line == 1:
-                cell = float(chunk.split()[1])
-            elif line == 1:
-                nodata = float(chunk.split()[1])
+                cols = int(fileline.split()[1])
+            elif line == 2:
+                rows = int(fileline.split()[1])
+            elif line == 3:
+                xcorner = float(fileline.split()[1])
+            elif line == 4:
+                ycorner = float(fileline.split()[1])
+            elif line == 5:
+                cell = float(fileline.split()[1])
+            elif line == 6:
+                nodata = float(fileline.split()[1])
             line += 1
         else:
             if line == 7:
                 header = Header(cols = cols, rows = rows, type_raster = 'LL', xcorner = xcorner, ycorner = ycorner, cellsize = cell, map = map)
                 header.save()
-            row = Row(number = number_row, map = map)
+            lat = latitude(cell,number_row,ycorner,rows)
+            row = Row(number = number_row,latitude=lat, map = map)
             row.save()
-            analyze_section(row, chunk, nodata)
+            number_row += 1
+            analyze_section(row, fileline, nodata, cell, xcorner)
 
-def analyze_section(row, line, nodata):
+def analyze_section(row, line, nodata, cellsize, xcorner):
     col = 0
     data = line.split()
-    current = ''
+    current = ""
     start = -1
     end = 1
-    for i in range(len(data)):
-        if float(data[i]) != nodata
-        if current != data[i] & float(data[i]) != nodata & i != 0 :
-            section = Section(value=float(data[i]), start=start, end=i-1, row=row)
-            current = data[i]
+    for i in range(len(data)):        
+        #end to count
+        if (current != str(data[i])) & (start != -1) :
+            lon_start = longitude(cellsize,start,xcorner)
+            lon_end = longitude(cellsize,i-1,xcorner)
+            section = Section(value=float(data[i]), start=lon_start, end=lon_end, row=row)
+            section.save()            
+            print (str(i) + " - " + current + " - " + str(data[i]) + " - " + str(start))
+            print (str(row.latitude) + ": " + str(lon_start) + " - " + str(lon_end))
+            start = -1
+            current = ""
+        #start to count
+        if (start == -1) & (float(data[i]) != nodata) & (current != str(data[i])):
             start = i
+            current = str(data[i])
+        elif float(data[i]) == nodata:
+            start = -1
+            current = ""
+        
+
+def longitude(cellsize,col,xcorner):
+    return (col*cellsize)+xcorner
+
+def latitude(cellsize,row,ycorner,nrows):
+    return ((nrows-row)*cellsize)+ycorner
+            
