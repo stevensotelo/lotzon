6efc6455bc15798f3bbfc7a1bc0f15d055734bf3
stevensotelo <stevenbetancurt@hotmail.com>
2015-02-19 14:52:49 -0500

    fix slide lateral right

--- map_points/views.py
+++ map_points/views.py
@@ -36,21 +36,24 @@ def process_points(file, file_points):
     lt = -1
     ln = -1
     text = -1
+    value = -1
     number_line = 0
     file_name = os.path.join(settings.MAPS_POINTS, str(file_points.id) + '.json')
     file_writer = open(file_name, 'w+')
     content = ""
     for l in file:
         line=str(l).replace("b'",'').replace("\\r",'').replace("\\n'",'')
-        vals = re.split("\;",str(line).lower())
-        if number_line == 0:
-            group = vals.index("group")
-            id = vals.index("id")
-            lt = vals.index("latitude")
-            ln = vals.index("longitude")
-            text = vals.index("text")
-        else:
-            content += '{"g":"' + vals[group] + '","i":"' + vals[id] + '","lt":"' + vals[lt] + '","ln":"' + vals[ln] + '","t":"' + vals[text] +'"},\n'
+        if line != "":
+            vals = re.split("\;",str(line).lower())
+            if number_line == 0:
+                group = vals.index("group")
+                id = vals.index("id")
+                lt = vals.index("latitude")
+                ln = vals.index("longitude")
+                text = vals.index("text")
+                value = vals.index("value")
+            else:
+                content += '{"g":"' + vals[group] + '","i":"' + vals[id] + '","lt":"' + vals[lt] + '","ln":"' + vals[ln] + '","t":"' + vals[text] +'","v":"' + vals[value] + '"},\n'
         number_line += 1
     file_writer.write('{"points":[' + content[:len(content)-2] + '],\n"header":{"count":"' + str(number_line)  + '"}}')
     file_writer.close()
